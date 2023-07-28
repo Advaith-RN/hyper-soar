504b0b92621007bcf6c63e5bda8100075ae947ac
RazerAds <adunair123@gmail.com>
2023-07-28 07:08:39 +0530

    made drone pages responsive

--- src/pages/SeriesB.jsx
+++ src/pages/SeriesB.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const SeriesB = () => {
   return (
-    <div className='h-screen overflow-y-hidden'>
+    <div className='md:h-screen'>
         <div className="w-full flex flex-row justify-between mt-[1%]">
             <Link to='/'>
                 <img
@@ -15,21 +15,21 @@ const SeriesB = () => {
             </Link>
         </div>
         <div className='h-[91%] justify-center flex flex-col items-center'>
-            <div className='h-[70%] w-[95%] flex flex-row'>
-                <div className='w-[50%]'>
+            <div className='h-[70%] w-[95%] flex md:flex-row flex-col'>
+                <div className='w-full md:w-[50%]'>
                     <img src='../products/droneB.png' className='w-[90%] object-contain'/>
                 </div>
-                <div className='w-[50%] flex flex-col justify-center items-center'>
+                <div className='w-full md:w-[50%] flex flex-col justify-center items-center'>
                     <p className='text-2xl font-medium text-center w-full'>This drone prioritizes camera quality, and ensures that your next footage is the best one yet.</p>
                     <div className='flex flex-row mt-[20%] gap-16'>
-                        <img src='../categories/cmos.png' className='w-36'/>
-                        <img src='../categories/fivek.png' className='w-36'/>
-                        <img src='../categories/stable.png' className='w-36'/>
+                        <img src='../categories/cmos.png' className='w-24 md:w-36'/>
+                        <img src='../categories/fivek.png' className='w-24 md:w-36'/>
+                        <img src='../categories/stable.png' className='w-24 md:w-36'/>
                     </div>
                 </div>
             </div>
-            <div className='h-[30%] w-[95%] bg-dark-gray mb-[2%]'>
-                <h1 className='text-white font-integral text-9xl pl-[3%]'>Cyclops</h1>
+            <div className='h-[30%] w-[95%] bg-dark-gray mb-[2%] md:mt-0 mt-[10%] p-4'>
+                <h1 className='text-white font-integral text-7xl md:text-9xl pl-[3%]'>Cyclops</h1>
                 <h2 className='text-green pl-[3%] font-inter mt-6 text-3xl'>Series B</h2>
             </div>
         </div>
